4fcba95d40ff05e2dbeffebaca65bef17b27611d
L C <73780698+Lnchi@users.noreply.github.com>
2023-10-19 17:36:49 +0100

    Update DBConnection.java

--- src/mvc/util/DBConnection.java
+++ src/mvc/util/DBConnection.java
@@ -2,21 +2,22 @@ package mvc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
  public class DBConnection {
+	// Méthode pour créer une connexion à la base de données
 	public static Connection createConnection()
 	 { Connection con = null;
-	  // URL MySQL suivie du nom de la base de donn�es
+	  // URL MySQL suivie du nom de la base de données
 	     String url = "jdbc:mysql://localhost:3306/BiblioNet?"
 	     	       	+ "zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC " ;
          String username = "root";// Nom d'utilisateur MySQL
 	     String password = "lynda"; // Mot de pass MySQL 
 	     try 
-	     { try // chargement des pilotes MySQL
+	     { try // Chargement des pilotes MySQL (chargement du pilote JDBC)
 	         {Class.forName("com.mysql.cj.jdbc.Driver"); 
 	         } 
 	         catch (ClassNotFoundException e)
 	         { e.printStackTrace();
 	         }       
-	        //Connexion � la base de donn�es MySQL
+	        //Connexion à la base de données MySQL
 	         con = DriverManager.getConnection(url, username, password); 
 	         System.out.println("Impression de l'objet de connexion"+con);
 	     } catch (Exception e) 
