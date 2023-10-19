c4eb40207dae7b1ccb9780836dcff1748b8d6959
L C <73780698+Lnchi@users.noreply.github.com>
2023-10-19 15:28:52 +0100

    Update Author.java

--- src/mvc/controller/Author.java
+++ src/mvc/controller/Author.java
@@ -15,28 +15,30 @@ public class Author extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	public Author() {
 		super();} 
+/**G√©rer une requ√™te HTTP POST pour ajouter des donn√©es d'auteur √† partir d'un formulaire web, les stocker dans un objet Java (Java Bean),
+puis les ins√©rer dans une base de donn√©es via une classe DAO (Data Access Object). En cas de succ√®s ou d'√©chec, des messages sont renvoy√©s √† l'utilisateur. */
 protected void doPost(HttpServletRequest request, HttpServletResponse
   response) throws ServletException, IOException { 
-        // Copie de tous les paramËtres d'entrÈe dans des variables locales
+        // Copie de tous les param√®tres d'entr√©e dans des variables locales
 		String num = request.getParameter("num");
 		String lastName = request.getParameter("lastName");
 		String firstName = request.getParameter("firstName");
 		String dob = request.getParameter("dob");
   UserAuthorBookBean authorBean = new UserAuthorBookBean();  
-  //Utilisation de Java Beans qui est Un moyen le plus simple de jouer avec un groupe de donnÈes associÈes
+  //Utilisation de Java Beans qui est Un moyen le plus simple de jouer avec un groupe de donn√©es associ√©es
   authorBean.setNum(num);
   authorBean.setLastName(lastName);
   authorBean.setFirstName(firstName);
   authorBean.setDob(dob);
-  //La logique de base de l'ajout d'un auteur est prÈsente ici.
-  //Nous allons insÈrer les donnÈes de l'auteur dans la base de donnÈes. 
+  //La logique de base de l'ajout d'un auteur est pr√©sente ici.
+  //Nous allons ins√©rer les donn√©es de l'auteur dans la base de donn√©es. 
   AuthorDao authorDao = new AuthorDao();
   String authorAdded =  authorDao.addAuthor(authorBean);
-  if(authorAdded.equals("Success"))  // En cas de succËs, on affiche un message de succËs sur la page 
+  if(authorAdded.equals("Success"))  // En cas de succ√®s, on affiche un message de succ√®s sur la page 
 		{ request.getRequestDispatcher("addAuthor.jsp").forward(request, response); 
 	  }
   else { request.setAttribute("messError", authorAdded); 
-//En cas d'Èchec,on affiche un message d'erreur ‡ l'dministrateur.
+//En cas d'√©chec,on affiche un message d'erreur √† l'dministrateur.
   request.getRequestDispatcher("AddAuthor.jsp").forward(request, response); }} }
 	
 
